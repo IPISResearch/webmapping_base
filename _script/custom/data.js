54bbababe33837c1622c604abaa70d6e49631a5c
Steffest <dev@stef.be>
2020-03-03 11:15:08 +0100

    Add GeoJson Utility features

--- _script/custom/data.js
+++ _script/custom/data.js
@@ -20,6 +20,42 @@ var Data = function () {
 		return years;
 	};
 
+	me.featureCollection = function() {
+		return {
+			"type": "FeatureCollection",
+			"crs": {
+				"type": "name",
+				"properties": {
+					"name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+				}
+			},
+			"features": []
+		}
+	};
+
+	me.featurePoint = function(lat, lon) {
+		return {
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [lon, lat]
+			}
+		}
+	};
+
+	me.featureLine = function(lat1,lon1,lat2,lon2){
+		return {
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [[lon1,lat1],[lon2,lat2]]
+			}
+		}
+	};
+	
+	
     return me;
 
 
