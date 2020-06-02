390dea169d896b6ad17555ebe4030edf8d4c7767
Steffest <dev@stef.be>
2020-06-02 13:15:46 +0200

    Mapbox version update

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 			},
 			mapbox:{
                 src: [
-                    'core/_script/lib/mapboxgl.0.43.0.js'
+                    'core/_script/lib/mapboxgl.1.5.1.js'
                 ],
                 dest: 'build/_script/mapboxgl.js'
             }
